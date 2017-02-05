9410ad822a40b3616a91fd94777a50617ec735a5
sverrir <sverrir1993@gmail.com>
2017-02-05 10:44:54 +0000

    balancing and character controller

---
+++ Irresponsible Constructors/Assets/FloorController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour {
+
+    [SerializeField]
+    Transform startPoint;
+
+    [SerializeField]
+    Transform endPoint;
+
+    public Vector2 RightDirection { get { return (Vector2)(endPoint.position - startPoint.position).normalized; } }
+    public Vector2 LeftDirection { get { return (Vector2)(startPoint.position - endPoint.position).normalized; } }
+
+    public Vector2 Normal { get { return new Vector2(-RightDirection.y, RightDirection.x).normalized; } }
+
+    public Vector2 StartPosition { get { return startPoint.position; } }
+
+    public Vector2 EndPosition { get { return endPoint.position; } }
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Rotate(0, 0, 5 * Time.deltaTime);
+	}
+
+    public Vector2 GetPositionOnFloor(float lerpValue)
+    {
+        return Vector2.Lerp(StartPosition, EndPosition, lerpValue);
+    }
+}
