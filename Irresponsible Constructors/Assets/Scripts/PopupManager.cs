e37c95381e2eb01b50190f4f9fa52c561dc3d6b5
einargizz <einarog05@ru.is>
2017-02-12 16:45:08 +0000

    Added Popup animations for score changes

---
+++ Irresponsible Constructors/Assets/Scripts/PopupManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupManager : MonoBehaviour {
+
+    public Animator anim;
+    public RectTransform oneUp;
+    public RectTransform oneDown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            oneUp.position = Input.mousePosition;
+            anim.SetTrigger("OneUp");
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            oneDown.position = Input.mousePosition;
+            anim.SetTrigger("OneDown");
+        }
+    }
+}
